98ff4396f6ace512ad2877b53d04aa4efc8564bb
Matt Hintzke <mhintzke.wsu@hotmail.com>
2013-09-28 12:40:30 -0700

    Init Commit

---
+++ CS322Final/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XmlConfiguration;
+using System.IO;
+
+namespace CS322Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlTextReader xtr = new XmlTextReader("DFA.xml");
+            int indents = 0, layer = 0;
+
+            while (xtr.Read())
+            {
+                //Console.WriteLine(indents);
+                layer = indents;
+               // while (layer != 0) { Console.Write("  "); layer--; }
+                
+                switch (xtr.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.WriteLine("<" + xtr.Name + ">");
+                        indents++;
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine("\t"+xtr.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+
+                        Console.WriteLine("</" + xtr.Name + ">");
+                        Console.Write('\n');
+                        indents--;
+                        break;
+                }
+
+            }
+        }
+    }
+}
